4cb57992c35502fcc176d4d16e639b5d4eee8bfa
HectorSauR <hector.sauceda.01@gmail.com>
2023-09-16 00:16:34 -0700

    practica 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practicas_computo_nube/src/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Practica 4',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SplashScreen(),
+      // home: const MaterialApp(title: 'Pracitca 04',home: HomePage(),),
     );
   }
 }
